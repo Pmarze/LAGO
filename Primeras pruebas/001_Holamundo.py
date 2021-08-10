32f7fd272012b9e604a1a65d9ed64c9d8ed954e3
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-08-09 22:17:42 -0600

    prueba de GPio en c

--- Primeras pruebas/001_Holamundo.py
+++ Primeras pruebas/001_Holamundo.py
@@ -1,13 +1,19 @@
-import RPi.GPIO as gp       # librería para utilizar los pines de raspberry pi
-import time                 # Librería para las pausas
-pin=4                       # pin del LED
-t=0.125                     # delay
-gp.setwarnings(False)       
+# Comandos de ejecucion
+# python 001_Holamundo.py
+
+import RPi.GPIO as gp       # libreria para utilizar los pines de raspberry pi
+import time                 # Libreria para las pausas
 gp.setmode(gp.BCM)          # Elegir tipo de numeracion BCM o BOARD
+pin=4                       # pin del LED en formato BCM
+t=0.125                     # delay en segundos
+
 gp.setup(pin,gp.OUT)        # Fijar el pin como salida
+
 for i in range(0,10):       # Ciclo para encender y apagar el LED
 	gp.output(pin,gp.HIGH)
 	time.sleep(t)           # Tiempo de espera t segundos
 	gp.output(pin,gp.LOW)
 	time.sleep(t)
-gp.cleanup()                # Se limpia la conexión utilizada
+gp.cleanup()                # Se limpia la conexion utilizada
+
+gp.setwarnings(False)      	# Evitar mensajes de error en la terminal
---
+++ Primeras pruebas/002_Holamundo.c
@@ -0,0 +1,20 @@
+// Comandos de ejecución
+// gcc 002_Holamundo.c -o 002 -lwiringPi
+// ./002
+
+#include <wiringPi.h>		// librería para utilizar los pines de Raspberry pi
+
+#define pin 7				// pin del LED en formato BOARD
+
+void main(){
+	int t=125;				// delay en micro segundos
+	int x;
+	wiringPiSetup();		// definir los comandos
+	pinMode(pin,OUTPUT);	// Fijar el pin como salida
+	for (x=0;x<10;x++){		// Ciclo para encender y apagar el LEDc
+		digitalWrite(pin,HIGH);
+		delay(t);
+		digitalWrite(pin,LOW);
+		delay(t);
+	}
+}
