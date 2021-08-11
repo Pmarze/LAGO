4134bc00ee265379fd38b52b7402068475f71b23
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-08-11 01:20:32 -0600

    Correcciones de identación

--- Primeras pruebas/001_Holamundo.py
+++ Primeras pruebas/001_Holamundo.py
@@ -1,19 +1,19 @@
 # Comandos de ejecucion
 # python 001_Holamundo.py
 
-import RPi.GPIO as gp       # libreria para utilizar los pines de raspberry pi
-import time                 # Libreria para las pausas
-gp.setmode(gp.BCM)          # Elegir tipo de numeracion BCM o BOARD
-pin=4                       # pin del LED en formato BCM
-t=0.125                     # delay en segundos
+import RPi.GPIO as gp		# libreria para utilizar los pines de raspberry pi
+import time					# Libreria para las pausas
+gp.setmode(gp.BCM)			# Elegir tipo de numeracion BCM o BOARD
+pin=4						# pin del LED en formato BCM
+t=0.125						# delay en segundos
 
-gp.setup(pin,gp.OUT)        # Fijar el pin como salida
+gp.setup(pin,gp.OUT)		# Fijar el pin como salida
 
-for i in range(0,10):       # Ciclo para encender y apagar el LED
+for i in range(0,10):		# Ciclo para encender y apagar el LED
 	gp.output(pin,gp.HIGH)
 	time.sleep(t)			# Tiempo de espera t segundos
 	gp.output(pin,gp.LOW)
 	time.sleep(t)
-gp.cleanup()                # Se limpia la conexion utilizada
+gp.cleanup()				# Se limpia la conexion utilizada
 
-gp.setwarnings(False)      	# Evitar mensajes de error en la terminal
+gp.setwarnings(False)		# Evitar mensajes de error en la terminal
