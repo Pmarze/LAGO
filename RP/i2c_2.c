737989b5e9f950486856c923f4d18eeaf3c6f490
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-11-17 12:25:51 -0600

    Update i2c_2.c

--- RP/i2c_2.c
+++ RP/i2c_2.c
@@ -133,11 +133,11 @@ int main(int argc, char *argv[])
     i2c_smbus_write_byte_data(fd, 0x00, SET_SEG_REMAP | 0x1 );
     i2c_smbus_write_byte_data(fd, 0x00, COM_SCAN_DIR_OP);
     i2c_smbus_write_byte_data(fd, 0x00, SET_COM_PIN_CFG);
-    i2c_smbus_write_byte_data(fd, 0x00, 0x02);
+    i2c_smbus_write_byte_data(fd, 0x00, 0x12);
     i2c_smbus_write_byte_data(fd, 0x00, SET_CONTRAST);
-    i2c_smbus_write_byte_data(fd, 0x00, 0x8f);
+    i2c_smbus_write_byte_data(fd, 0x00, 0x7f);
     i2c_smbus_write_byte_data(fd, 0x00, SET_PRECHARGE);
-    i2c_smbus_write_byte_data(fd, 0x00, 0xf1);
+    i2c_smbus_write_byte_data(fd, 0x00, 0xc2);
     i2c_smbus_write_byte_data(fd, 0x00, 0xd8);
     i2c_smbus_write_byte_data(fd, 0x00, SET_START_LINE);
     i2c_smbus_write_byte_data(fd, 0x00, SET_ENTIRE_ON);
@@ -182,7 +182,7 @@ int main(int argc, char *argv[])
     sleep(1);
 
     for( int i = 0; i< 32; i++){
-        i2c_smbus_write_byte_data(fd, 0x40, 0xf0);
+        i2c_smbus_write_byte_data(fd, 0x40, 0x0f);
     }
 
     /* Write to redpitaya eeprom */
