70d84a663b134ec5eca406b8c51e95e811f2de4b
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-11-20 17:43:26 -0600

    Update lcd_bmp.c

--- RP/lcd_bmp.c
+++ RP/lcd_bmp.c
@@ -1,64 +1,13 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <linux/ioctl.h>
-#include <sys/ioctl.h>
-#include <linux/i2c-dev.h>
-#include <string.h>
+#include "bmp180.h"
 #include <unistd.h>
-#include <errno.h>
-#include <stdint.h>
-#include "font.h"
+#include <stdio.h>
 
-#define OLED96_ADDR     0x3c
-
-#define PAGESIZE        32
-/* eeprom size on a redpitaya */
-
-static int iic_read(char *buffer, int offset, int size, int fd);
-static int iic_write(char *data, int offset, int size, int fd);
-
-/*
-* File descriptors
-*/
-int fd;
-
-int main(int argc, char *argv[])
-{
-    int status;
-    fd = open("/dev/i2c-0", O_RDWR);
-
-    if(fd < 0)
-    {
-        printf("Cannot open the IIC device\n");
-        return 1;
-    }
-
-    status = ioctl(fd, I2C_SLAVE, OLED96_ADDR);
-    if(status < 0)
-    {
-        printf("Unable to set the OLED96 address\n");
-        return -1;
-    }
-    char buf[10];
-    buf[0] = SET_OSC_FREQ;
-    buf[1] = DISPLAY_OFF;
-    if ( i2c_smbus_write_byte_data(fd, 0x00, DISPLAY_OFF) < 0 )
-    {
-        printf("Unable to send commands\n");
-        printf("errno: %i %s\n",errno,strerror(errno));
-        return -1;
-    }
-
+int main(int argc, char **argv){
 	char *i2c_device = "/dev/i2c-0";
 	int address = 0x77;
 	
 	void *bmp = bmp180_init(address, i2c_device);
 	
-
-    initialize(fd);
-    clear_lcd(fd);
-
 	bmp180_eprom_t eprom;
 	bmp180_dump_eprom(bmp, &eprom);
 	
@@ -77,6 +26,6 @@ int main(int argc, char *argv[])
 	
 		bmp180_close(bmp);
 	}
-
-
+	
+	return 0;
 }
