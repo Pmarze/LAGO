fa85731063257db7dd7b045426e0ed1e9e4d949a
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-01-04 00:45:12 -0600

    Documentación para Doxygen
    Se añaden comentarios para presentar la documentación con Doxygen

--- RP/functions.h
+++ RP/functions.h
@@ -1,3 +1,26 @@
+  /**
+    * @file functions.h
+    * @version 1.0
+    * @date 16/12/2021
+    * @author Pablo M. Zeceña
+    * @brief Library containing the necessary functions to make the SSD1306 LCD display and the BMP180 sensor work properly
+    * @code
+      int main(int argc, char **argv){
+	      fun_inic_disp();
+	      if(bmp != NULL){
+          fun_page_data(fd, 0x00, 0x00);
+          fun_println(fd, "Hello World");
+          sleep(3);
+          fun_clear_lcd(fd);
+	      }
+	      fun_close_disp(bmp,fd);
+	      return 0;
+      }
+      // The LCD Show: 
+      // Hello World
+    * @endcode
+  */
+
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
@@ -11,7 +34,6 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdint.h>
-//#include "font.h"
 
 #define OLED96_ADDR     0x3c
 #define PAGESIZE        32
@@ -19,16 +41,62 @@
 int fd;
 void *bmp;
 
+  /**
+    * @brief Information about the location to set the pointer
+    * @param fd Indicates the direction of the dispositive
+    * @param page_a Indicates the page number (row) where the pointer is to be placed
+    * @param column_a Indicates the column number where the pointer is to be placed
+    */
 void fun_page_data(int, int, int);
+  /**
+    * @brief Function to define in a single line the initial coordinates where the text is to be displayed. 
+    * @param fd Indicates the direction of the dispositive
+    */
 void fun_initialize(int);
+  /**
+    * @brief Clears each point of the screen one by one, sending an empty bit.
+    * @param fd Indicates the direction of the dispositive
+    */
 void fun_clear_lcd(int);
+  /**
+    * @brief Returns the function that represents on the display the number entered in the function
+    * @param fd Indicates the direction of the dispositive
+    * @param a Number to be displayed on the LCD screen
+    */
 void fun_lcd_num(int, int);
-
-void fun_digits(int, int);
-void fun_bienv(int);
-void fun_data(void *, int);
-void fun_inic_disp();
-void fun_close_disp(void *, int);
+  /**
+    * @brief Displays on the LCD screen, the text entered to the function between quotation marks.
+    * @param fd Indicates the direction of the dispositive
+    * @param arr Text to be displayed on the LCD
+    */
 void fun_println(int, char []);
+
+  /**
+    * @brief Separates a float number into individual int
+    * @param fd Indicates the direction of the dispositive
+    * @param num Number to be separated
+    */
+void fun_digits(int, int);
+  /**
+    * @brief Displays a welcome message at startup
+    * @param fd Indicates the direction of the dispositive
+    */
+void fun_bienv(int);
+  /**
+    * @brief Obtains ambient temperature, pressure and altitude from the BMP180 sensor and displays these data on the LCD screen
+    * @param bmp Indicates the address of the BMP180 sensor
+    * @param fd Indicates the direction of the dispositive
+    */
+void fun_data(void *, int);
+  /**
+  * @brief Initializes all devices
+  */
+void fun_inic_disp();
+  /**
+    * @brief Close all devices
+    * @param bmp Indicates the address of the BMP180 sensor
+    * @param fd Indicates the direction of the dispositive
+    */
+void fun_close_disp(void *, int);
 
 #endif /*FUNCTIONS_H*/
