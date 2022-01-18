b747a851ce50bd4a590124cc666cdd31b13b46a5
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-01-18 01:35:59 -0600

    test

--- lcd_bmp/font.h
+++ lcd_bmp/font.h
@@ -220,8 +220,8 @@ uint8_t z_MI[5] = {0x44,0x64,0x54,0x4C,0x44};
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void character(int fd, uint8_t C[]){
-	for(int i=0 ; i<4; i++){
+void character(int fd, uint8_t C[5]){
+	for(int i=0 ; i<5; i++){
         i2c_smbus_write_byte_data(fd, SET_START_LINE, C[i]);
     }
     i2c_smbus_write_byte_data(fd, SET_START_LINE, 0x00); 
