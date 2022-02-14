fe13a4e9383a700200ed539c75f7a83cd3894aa5
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-14 14:14:33 -0600

    Update functions.h

--- RP/pulse_acquire/include/functions.h
+++ RP/pulse_acquire/include/functions.h
@@ -89,7 +89,7 @@ void fun_bienv(int);
     * @param bmp Indicates the address of the BMP180 sensor
     * @param fd Indicates the direction of the dispositive
     */
-void fun_data(void *, int, int, FILE *);
+void fun_data(void *, int, int);
   /**
   * @brief Initializes all devices
   */
