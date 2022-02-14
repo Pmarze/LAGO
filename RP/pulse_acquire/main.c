705832fd5c7cccc330ec887e86790113640e8e65
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-14 11:14:11 -0600

    finished?

--- RP/pulse_acquire/main.c
+++ RP/pulse_acquire/main.c
@@ -143,7 +143,7 @@ int main(int argc, char **argv)
     clock_gettime(CLOCK_REALTIME, &LTClock);
     
     pa_InitDataFile( pa_data_file ,1);
-    pa_InitDataFile( pa_data_file ,2);
+    pa_InitDataFile( pa_data_file2 ,2);
     
     pa_LogFileEntry( pa_log_file, "Acquisition started" );
     
