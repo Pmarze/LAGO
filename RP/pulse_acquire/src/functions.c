93309cc172541f28635c3a144f37e9a7f07f1c13
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-16 14:42:06 -0600

    test button

--- RP/pulse_acquire/src/functions.c
+++ RP/pulse_acquire/src/functions.c
@@ -55,6 +55,7 @@ void fun_initialize(int fd){
     i2c_smbus_write_byte_data(fd, 0x00, DISPLAY_NORMAL);
     i2c_smbus_write_byte_data(fd, 0x00, DEACT_SCROLL);
     i2c_smbus_write_byte_data(fd, 0x00, DISPLAY_ON);
+	fun_setbutton(5);
 }
 
 
@@ -107,7 +108,6 @@ void fun_data(void *bmp, int fd, int siesta){
     int tempera;
     int altu;
 
-	fun_setbutton(5);
 	while(1){
 		fun_buttonsgnl(5);
 	}
