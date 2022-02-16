eb653ab1c35ea71fc712192bf94c45b2fc629296
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-16 14:34:49 -0600

    first push botton test in pulse acquire

--- RP/pulse_acquire/include/rp_m.h
+++ RP/pulse_acquire/include/rp_m.h
@@ -242,7 +242,71 @@ typedef struct {
     int32_t  fe_ch2_hi_offs; //!< Front end DC offset, channel B
 } rp_calib_params_t;
 
+typedef enum {
+    RP_LOW, //!< Low state
+    RP_HIGH //!< High state
+} rp_pinState_t;
 
+int rp_DpinSetDirection(rp_dpin_t pin, rp_pinDirection_t direction) {
+    uint32_t tmp;
+    if (pin < RP_DIO0_P) {
+        // LEDS
+        if (direction == RP_OUT)  return RP_OK;
+        else                      return RP_ELID;
+    } else if (pin < RP_DIO0_N) {
+        // DIO_P
+        pin -= RP_DIO0_P;
+        tmp = ioread32(&hk->ex_cd_p);
+        iowrite32((tmp & ~(1 << pin)) | ((direction << pin) & (1 << pin)), &hk->ex_cd_p);
+    } else {
+        // DIO_N
+        pin -= RP_DIO0_N;
+        tmp = ioread32(&hk->ex_cd_n);
+        iowrite32((tmp & ~(1 << pin)) | ((direction << pin) & (1 << pin)), &hk->ex_cd_n);
+    }
+    return RP_OK;
+}
+
+int rp_DpinGetState(rp_dpin_t pin, rp_pinState_t* state) {
+    if (pin < RP_DIO0_P) {
+        // LEDS
+        *state = (ioread32(&hk->led_control) >> pin) & 0x1;
+    } else if (pin < RP_DIO0_N) {
+        // DIO_P
+        pin -= RP_DIO0_P;
+        *state = (ioread32(&hk->ex_ci_p) >> pin) & 0x1;
+    } else {
+        // DIO_N
+        pin -= RP_DIO0_N;
+        *state = (ioread32(&hk->ex_ci_n) >> pin) & 0x1;
+    }
+    return RP_OK;
+}
+
+int rp_DpinSetState(rp_dpin_t pin, rp_pinState_t state) {
+    uint32_t tmp;
+    rp_pinDirection_t direction;
+    rp_DpinGetDirection(pin, &direction);
+    if (!direction) {
+        return RP_EWIP;
+    }
+    if (pin < RP_DIO0_P) {
+        // LEDS
+        tmp = ioread32(&hk->led_control);
+        iowrite32((tmp & ~(1 << pin)) | ((state << pin) & (1 << pin)), &hk->led_control);
+    } else if (pin < RP_DIO0_N) {
+        // DIO_P
+        pin -= RP_DIO0_P;
+        tmp = ioread32(&hk->ex_co_p);
+        iowrite32((tmp & ~(1 << pin)) | ((state << pin) & (1 << pin)), &hk->ex_co_p);
+    } else {
+        // DIO_N
+        pin -= RP_DIO0_N;
+        tmp = ioread32(&hk->ex_co_n);
+        iowrite32((tmp & ~(1 << pin)) | ((state << pin) & (1 << pin)), &hk->ex_co_n);
+    }
+    return RP_OK;
+}
 
 #ifdef __cplusplus
 }
