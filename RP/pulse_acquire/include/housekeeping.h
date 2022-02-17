406c9557d51be070d8573ddd0b78567c83972488
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-17 02:23:53 -0600

    Update housekeeping.h

--- RP/pulse_acquire/include/housekeeping.h
+++ RP/pulse_acquire/include/housekeeping.h
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "rp_cross.h"
+#include "rp_m.h"
 // Base Housekeeping address
 static const int HOUSEKEEPING_BASE_ADDR = 0x00000000;
 #ifdef Z20_250_12
