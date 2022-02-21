c7d7b96ddb6aa10f67d20d97bf25c989fb909fa0
Pmarze <53276103+Pmarze@users.noreply.github.com>
2022-02-20 19:11:11 -0600

    commented versiont of the push button functionality

--- RP/push_botton/rp_pushbotton.c
+++ RP/push_botton/rp_pushbotton.c
@@ -4,6 +4,7 @@
 #include "rp.h"
 
 int main (int argc, char **argv) {
+    // Variable used to know the state of the push button
     rp_pinState_t state;
 
     // Initialization of API
@@ -17,6 +18,7 @@ int main (int argc, char **argv) {
 
     // transfer each input state to the corresponding LED state
     while (1) {
+        // state is a "enum" structure only with RP_LOW and RP_HIGH
         rp_DpinGetState (RP_DIO5_N, &state);
         rp_DpinSetState (RP_LED5, state);
     }
